cc9fb9ae1982295602ec2db8b387103a613d943f
ivveee <ivee@list.ru>
2013-09-05 19:42:48 +0400

    wormsDig! and dest part

--- src/LWorm.java
+++ src/LWorm.java
@@ -113,12 +113,13 @@ class LWorm extends LBody{
         float direction = 0f;
         if(parent.random(-1,1)>0) direction= 1.f; else direction= -1.f;
         normalize = new Vec2(normalize.y*direction,normalize.x*direction);}
+    if( normalize.y>0)normalize = new Vec2(normalize.x,-normalize.y);
     //direction = 1;
        //Vec2 normalizeNorm = new Vec2(direction*normalize.y,direction*normalize.x);
     Vec2 size = new Vec2(parent.iPixDefaultHalfSize,parent.iPixDefaultHalfSize);
     Vec2 coord = Head.add(normalize.mul(parent.iPixDefaultHalfSize));
-    //parent.fill(parent.color(255,0,0));
-      //   parent.rect(coord.x, coord.y, size.x, size.y);
+    parent.fill(parent.color(255,0,0));
+        parent.rect(coord.x, coord.y, size.x, size.y);
     Vec2 pointCoordAndSize = parent.box2d.coordPixelsToWorld(coord.add(size));
     Vec2 pointCoord = parent.box2d.coordPixelsToWorld(coord);
     Vec2 lowerBound = new Vec2(luc.min(pointCoordAndSize.x,pointCoord.x), luc.min(pointCoordAndSize.y,pointCoord.y));
